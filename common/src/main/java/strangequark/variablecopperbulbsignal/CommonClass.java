e5a763fc207db5cf34f080bdd9861c5460f23576
Yasin  Bhimani <masteryasin@ymail.com>
2026-04-25 00:00:39 +0530

    port: update to 26.1

--- common/src/main/java/strangequark/variablecopperbulbsignal/CommonClass.java
+++ common/src/main/java/strangequark/variablecopperbulbsignal/CommonClass.java
@@ -13,11 +13,6 @@ public class CommonClass {
     // write the majority of your code here and load it from your loader specific projects. This example has some
     // code that gets invoked by the entry point of the loader specific projects.
     public static void init() {
-
-        Constants.LOG.info("Hello from Common init!");
-        Constants.LOG.info("The ID for diamonds is {}", BuiltInRegistries.ITEM.getKey(Items.DIAMOND));
-
         // Your mod initialization code goes here
-        Constants.LOG.info("Mod initialized successfully!");
     }
 }
--- common/src/main/java/strangequark/variablecopperbulbsignal/mixin/BulbBlockMixin.java
+++ common/src/main/java/strangequark/variablecopperbulbsignal/mixin/BulbBlockMixin.java
@@ -1,6 +1,7 @@
 package strangequark.variablecopperbulbsignal.mixin;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.CopperBulbBlock;
 import net.minecraft.world.level.block.state.BlockState;
@@ -12,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(CopperBulbBlock.class)
 public class BulbBlockMixin {
     @Inject(method = "getAnalogOutputSignal", at = @At("HEAD"), cancellable = true)
-    public void overrideComparatorOutput(BlockState state, Level level, BlockPos pos, CallbackInfoReturnable<Integer> cir){
+    public void overrideComparatorOutput(BlockState state, Level level, BlockPos pos, Direction direction, CallbackInfoReturnable<Integer> cir){
         cir.setReturnValue(state.getLightEmission());
         cir.cancel();
     }
