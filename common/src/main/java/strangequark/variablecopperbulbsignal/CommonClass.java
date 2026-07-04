2b147ecf816c9f92f205dc4f3e9d9828ff3818c1
Yasin  Bhimani <masteryasin@ymail.com>
2026-07-04 13:17:54 +0530

    port: update to 1.21.11
    Fixes: #3 Game crashes on startup | InvalidMixinException in BulbBlockMixin on 1.21.11

--- common/src/main/java/strangequark/variablecopperbulbsignal/CommonClass.java
+++ common/src/main/java/strangequark/variablecopperbulbsignal/CommonClass.java
@@ -1,8 +1,5 @@
 package strangequark.variablecopperbulbsignal;
 
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.item.Items;
-
 // This class is part of the common project meaning it is shared between all supported loaders. Code written here can only
 // import and access the vanilla codebase, libraries used by vanilla, and optionally third party libraries that provide
 // common compatible binaries. This means common code can not directly use loader specific concepts such as Forge events
@@ -13,10 +10,7 @@ public class CommonClass {
     // write the majority of your code here and load it from your loader specific projects. This example has some
     // code that gets invoked by the entry point of the loader specific projects.
     public static void init() {
-
         Constants.LOG.info("Hello from Common init!");
-        Constants.LOG.info("The ID for diamonds is {}", BuiltInRegistries.ITEM.getKey(Items.DIAMOND));
-
         // Your mod initialization code goes here
         Constants.LOG.info("Mod initialized successfully!");
     }
